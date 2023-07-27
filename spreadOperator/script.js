bcf74a007d0835251de962d5602af4c87165d7d6
Chandan Yadav <103582224+MeetFluorine@users.noreply.github.com>
2023-07-27 21:20:41 +0000

    update

--- spreadOperator/script.js
+++ spreadOperator/script.js
@@ -6,3 +6,22 @@ let arr = [10,20,30]
 // document.write(sum(arr))   - [10,20,30]
 
 document.write(sum(...arr));
+
+
+
+// merging two object using spread operator
+
+let obj1 ={
+    name:"chandan"
+}
+let obj2 ={
+    age:21
+}
+
+let obj3 = {
+   ... obj1  , ...obj2
+}
+
+document.write(obj3)
+
+console.log(obj3);
